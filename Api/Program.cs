47345a48c8f414d9941fb38b6a28e922550bafc4
adriano mesquita <sweden1244@gmail.com>
2022-11-15 13:06:19 +0100

    pokemon

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestSharp;
+
+RestClient pokeClient = new RestClient("https://pokeapi.co/api/v2/");
+
+RestRequest request = new("pokemon/snorlax");
+
+RestResponse response = pokeClient.GetAsync(request).Result;
+
+Console.WriteLine(response.StatusCode);
+
+Console.ReadLine();
